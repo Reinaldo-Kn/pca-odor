cb45b454bbbabb52f2bead8543518f45f150ea53
Reinaldo-Kn <reinaldokneto@gmail.com>
2024-09-12 15:43:50 -0300

    vecho "0.0.1" > VERSION

---
+++ auto_commit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+current_version=$(cat VERSION)
+
+#versão em parte ( major, minor, patch)
+IFS='.' read -r major minor patch <<< "$current_version"
+
+patch=$((patch+1))
+new_version="$major.$minor.$patch"
+
+echo "$new_version" > VERSION
+
+# commit automatico 
+git add .
+git commit -m "v$new_version"
+git push origin master
+
+#print nova versao
+echo "Nova versão: $new_version"
